ddfbc14a113c935f50b9d15a00a5d5f83096a9f0
megan <meganfdoyle@gmail.com>
2024-11-25 18:52:41 -0500

    Add random background color button

---
+++ todo.js
@@ -0,0 +1,58 @@
+//initial prompt
+let input = prompt('What would you like to do?');
+
+const list = [];
+
+//quit any time the user enters 'quit'
+while (input.toLowerCase() !== 'quit' && input.toLowerCase() !== 'q') {
+    //add an item
+    if (input.toLowerCase() === 'new') {
+        const newToDo = prompt('Enter new todo');
+        list.push(newToDo);
+        console.log(`${newToDo} added to list`);
+        // input = prompt('What would you like to do?');
+    }
+    //list all todos
+    else if (input.toLowerCase() === 'list') {
+        console.log(`******************`);
+        for (let item of list) {
+            console.log(`${list.indexOf(item)}: ${item}`);
+        }
+        console.log(`******************`);
+    } 
+    //remove a todo
+    else if (input.toLowerCase() === 'delete') {
+        let index = parseInt(prompt('Enter index of todo you wish to delete'));
+        //delete the value at that index
+        while (!list[index]) {
+            index = prompt('Enter a *valid* index of the todo you wish to delete');
+        }
+        if (!isNaN(index) && list[index]) {
+            let deleted = list.splice(index, 1);
+            console.log(`${deleted} was removed`);
+        }
+    }
+    //if the user types anything other than the above actions
+    input = prompt('What would you like to do?');
+}
+//when the user quits
+console.log('fin');
+
+
+const body = document.querySelector('body');
+const randomColorBtn = document.querySelector('#random');
+const h3 = document.querySelector('#h3Color');
+
+const pickColor = function () {
+    let r = Math.floor(Math.random() * 255);
+    let g = Math.floor(Math.random() * 255);
+    let b = Math.floor(Math.random() * 255);
+    return `rgb(${r}, ${g}, ${b})`;
+}
+
+
+randomColorBtn.addEventListener('click', function () {
+    const newColor = pickColor();
+    body.style.backgroundColor = newColor;
+    h3.innerText = newColor;
+});
